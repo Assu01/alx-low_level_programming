8e6b926d008af98147cfea5b7214f2d968a8b8e8
Asrat Asmelash <99473565+Assu01@users.noreply.github.com>
2022-03-10 13:00:22 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This uses program prints exactly the string
+ * "Programming is like building a multilingual puzzle
+ * followed by a new line.
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
