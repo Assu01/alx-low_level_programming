80a3616b5e304ce4276294ea3daf8bc017320d92
Asrat Asmelash <99473565+Assu01@users.noreply.github.com>
2022-03-15 13:17:45 +0300

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,29 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_sign - Print the sign of a number
  *
- * Return: Always 0.
+ * @n: Number
+ *
+ * Return: 1 (is greater that zero) OR 0 (is zero)
+ * OR -1 (is less that zero)
  */
-int main(void)
-{
-	int r;
 
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
 }
