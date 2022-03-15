068f03c4cf47b45fc121f62950902a1c13320bdc
Asrat Asmelash <99473565+Assu01@users.noreply.github.com>
2022-03-15 13:13:58 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,19 @@
-#include"main.h"
+#include "main.h"
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - This program prints the phrase Hello, World
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	_putchar('m');
-	_putchar('a');
-	_putchar('i');
-	_putchar('n');
+	char ch[] = "main";
+	int i;
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		_putchar(ch[i]);
+	}
+
 	_putchar('\n');
 	return (0);
 }
