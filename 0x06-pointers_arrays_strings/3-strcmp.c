126894297dfaff1aa70c4f268a71afe369980517
Asrat Asmelash <99473565+Assu01@users.noreply.github.com>
2022-03-23 11:03:43 +0300

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - This function compares two strings
+ * @s1: The first string to compare
+ * @s2: The second string to compare
+ * Return: The integer difference between s1 and s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	/* loop through both strings while s1 & s2 are not null */
+	for (i = 0; s1[i] != 0 && s2[i] != 0; i++)
+	{
+		/* find where characters don't match in strings */
+		if ((s1[i] - s2[i]) != 0)
+			break;
+	}
+
+	/* return difference between those characters */
+	return (s1[i] - s2[i]);
+}
