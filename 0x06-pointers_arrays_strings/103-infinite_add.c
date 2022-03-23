35d7e28d7035f873d3e52b81afbc5b8bca1c15a0
Asrat Asmelash <99473565+Assu01@users.noreply.github.com>
2022-03-23 21:44:58 +0300

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,65 +1,104 @@
 #include "main.h"
 
 /**
- * infinite_add - add 2 integers.
- * @n1: integer
- * @n2: integer
- * @r: buffer
- * size_r: size of r
- * Return: char
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
  */
-
-int _atoi(char *s)
+int _strlen(char *s)
 {
-	int sign = 1, resp = 0, firstNum;
+	char *p = s;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	while (*s)
+		s++;
+	return (s - p);
+}
+
+/**
+ * rev_string - reverses a string
+ * @s: string s
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int size = _strlen(s);
+	char temp;
+
+	while (i < size)
 	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
-		}
+		temp = *(s + i);
+		*(s + i) = *(s + size - 1);
+		*(s + size - 1) = temp;
+		i++;
+		size--;
 	}
-
-	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
-
-	return (sign * resp);
 }
 
-void int_to_string(int n)
+/**
+ * returnRes - changes pretotal to digit to be added
+ * @sum: pre-total
+ * @plusOne: flag to add one to res
+ * Return: returns digit to be placed into array
+ */
+int returnRes(int sum, int plusOne)
 {
-int divisor = 1, i, resp;
+	int res;
 
+	if (sum == 9 && plusOne)
+		res = 0;
+	else if ((sum >= 10 && plusOne) || (sum < 9 && plusOne))
+		res = (sum % 10) + 1;
+	else
+		res = sum % 10;
+	return (res);
+}
 
-for (i = 0; n / divisor > 9; i++)
+/**
+ * returnPlusOne - determines bool of plusOne
+ * @sum: pre-total
+ * @plusOne: flag to add one to res
+ * Return: 1 if true, 0 if false
+ */
+int returnPlusOne(int sum, int plusOne)
 {
-	divisor *= 10;
+	if (sum > 9)
+		plusOne = 1;
+	else if (sum == 9 && plusOne)
+		plusOne = 1;
+	else
+		plusOne = 0;
+	return (plusOne);
 }
 
-char str[i];
-
-for (int cmpt = 0; divisor >= 10; divisor /= 10, cmpt++)
-{
-	resp = n / divisor;
-	str[cmpt] = '0' + resp;
-	n = n - resp * divisor;
-}
-str[i] = ('0' + n);
-
-}
-
-
+/**
+ * infinite_add - function that adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer that the function will use to store the result
+ * @size_r: size of buffer
+ * Return: pointer to result
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int sum, a, b;
-    a = _atoi(n1);
-    b = _atoi(n2);
+	int sum, res, first, second, i = 0, plusOne = 0;
+	int len1 = _strlen(n1), len2 = _strlen(n2);
+	char *ptr = r;
 
-    sum = a + b;
-
-
+	while (len1 > 0 || len2 > 0)
+	{
+		first = len1 > 0 ? (*(n1 + len1 - 1) - '0') : 0;
+		second = len2 > 0 ? (*(n2 + len2 - 1) - '0') : 0;
+		sum = first + second;
+		res = returnRes(sum, plusOne);
+		plusOne = returnPlusOne(sum, plusOne);
+		*(ptr + i) = res + '0';
+		len1--;
+		len2--;
+		i++;
+	}
+	if (plusOne)
+		*(ptr + i) = 1 + '0';
+	ptr[++i] = '\0';
+	rev_string(ptr);
+	return ((size_r > _strlen(ptr)) ? ptr : 0);
 }
