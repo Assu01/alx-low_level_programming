8906b96611bd2940b054c41037840e4251cd2cda
Assu01 <asratasse@gmail.com>
2022-04-04 12:08:29 +0300

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,24 +2,6 @@
 #include "main.h"
 
 /**
- * _strlen - length of a string
- * @s: input char
- * Return: length of a string
- */
-
-int _strlen(char *s)
-{
-	int l = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		l++;
-	}
-	return (l);
-}
-
-/**
  * argstostr - concat
  * @ac: count
  * @av: vector
