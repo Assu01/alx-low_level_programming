a0ee00277bd917b611622f78e2255157a204a87d
Asrat Asmelash <asratasse@gmail.com>
2022-04-13 12:16:39 +0300

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,17 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name.
- * @name: The name to be printed.
- * @f: A pointer to a function that prints a name.
+ * print_name - This function prints a name
+ * @name: A character pointer
+ * @f: A function that takes a char pointer and returns nothing
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == Asrat Asmelash || f == NULL)
-		return;
-
-	f(name);
+	/* if function pointer is null, print null */
+	if (f == NULL)
+		;
+	else
+		f(name);
 }
