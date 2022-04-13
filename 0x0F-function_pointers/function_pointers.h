87fd0c2fe8093a7b81c913125a8d02d8b6578e53
Asrat Asmelash <asratasse@gmail.com>
2022-04-13 20:03:59 +0300

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#ifndef FUNCTION_POINTERS_
-#define FUNCTION_POINTERS_
-
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+#include <stdlib.h>
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
-#endif /* #ifndef FUNCTION_POINTERS_ */
+#endif
